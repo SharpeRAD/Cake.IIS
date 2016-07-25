5ad41e3cd9c7de2a90269d21c1ba8c9ed2401cd6
Rafal Maciąg <rafal.maciag@hotmail.com>
2016-07-25 12:02:40 +0200

    Small refactor

--- src/IIS.Tests/Tests/ApplicationPoolTests.cs
+++ src/IIS.Tests/Tests/ApplicationPoolTests.cs
@@ -10,23 +10,6 @@ using Cake.IIS.Tests.Utils;
 
 namespace Cake.IIS.Tests
 {
-    public class ApplicationTests
-    {
-        [Fact]
-        public void Should_Create_App()
-        {
-            // Arrange
-            var siteSettings = CakeHelper.GetWebsiteSettings();
-            var appSettings = CakeHelper.GetVirtualAppSettings();
-            CakeHelper.CreateWebsite(siteSettings);
-            
-
-            // Act
-            CakeHelper.CreateVirtualApplication(appSettings);
-
-            Assert.NotNull(CakeHelper.GetVirtualApplication(appSettings.ParentWebSite, appSettings.Name));
-        }
-    }
     public class ApplicationPoolTests
     {
         [Fact]
---
+++ src/IIS.Tests/Tests/ApplicationTests.cs
@@ -0,0 +1,23 @@
+using Cake.IIS.Tests.Utils;
+using Xunit;
+
+namespace Cake.IIS.Tests
+{
+    public class ApplicationTests
+    {
+        [Fact]
+        public void Should_Create_App()
+        {
+            // Arrange
+            var siteSettings = CakeHelper.GetWebsiteSettings();
+            var appSettings = CakeHelper.GetVirtualAppSettings();
+            CakeHelper.CreateWebsite(siteSettings);
+            
+
+            // Act
+            CakeHelper.CreateVirtualApplication(appSettings);
+
+            Assert.NotNull(CakeHelper.GetVirtualApplication(appSettings.ParentWebSite, appSettings.Name));
+        }
+    }
+}
